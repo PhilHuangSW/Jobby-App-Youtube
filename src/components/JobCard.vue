125d31e6b1bfb15fbf1fda2bc168433e550f2428
Philip Huang <philip.sw.huang@gmail.com>
2020-10-28 22:51:34 -0700

    Created a JobCard component and replaced in Home

---
+++ src/components/JobCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-col>
+        <b-card 
+          title="This is a card" 
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job-info"
+          img-top
+          tag="article"
+          style="max-width:20rem"
+          class="mb-2"
+          footer="Card Footer"
+          >
+          <b-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum alias molestiae dicta.
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,82 +1,22 @@
 <template>
   <b-container>
     <b-row align-v="center">
-      <b-col md="3">
-        <b-card 
-          title="This is a card" 
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Job-info"
-          img-top
-          tag="article"
-          style="max-width:20rem"
-          class="mb-2">
-          <b-card-text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum alias molestiae dicta.
-          </b-card-text>
-          <b-button variant="primary">Apply</b-button>
-        </b-card>
-      </b-col>
-
-      <b-col md="3">
-        <b-card 
-          title="This is a card" 
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Job-info"
-          img-top
-          tag="article"
-          style="max-width:20rem"
-          class="mb-2">
-          <b-card-text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum alias molestiae dicta.
-          </b-card-text>
-          <b-button variant="primary">Apply</b-button>
-        </b-card>
-      </b-col>
-
-      <b-col md="3">
-        <b-card 
-          title="This is a card" 
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Job-info"
-          img-top
-          tag="article"
-          style="max-width:20rem"
-          class="mb-2">
-          <b-card-text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum alias molestiae dicta.
-          </b-card-text>
-          <b-button variant="primary">Apply</b-button>
-        </b-card>
-      </b-col>
-
-      <b-col md="3">
-        <b-card 
-          title="This is a card" 
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Job-info"
-          img-top
-          tag="article"
-          style="max-width:20rem"
-          class="mb-2">
-          <b-card-text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum alias molestiae dicta.
-          </b-card-text>
-          <b-button variant="primary">Apply</b-button>
-        </b-card>
-      </b-col>
-
-
+      <job-card></job-card>
+      <job-card></job-card>
+      <job-card></job-card>
+      <job-card></job-card>
     </b-row>
   </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
+import JobCard from '@/components/JobCard.vue'
 
 export default {
   name: "Home",
   components: {
-    
+    'job-card' : JobCard
   }
 };
 </script>
